01ad933f1780746545b02e9becae676aa42a822e
Ali Zaiter <152106542+alizaiter777@users.noreply.github.com>
2024-10-31 00:58:37 +0200

    Update exc.py

--- exc.py
+++ exc.py
@@ -1,9 +1,9 @@
-x=int(input("Enter a number : "))
+nbr=int(input("Enter a number : "))
 a=0
 b=1
 print(a)
 print(b)
-for i in range (x):
+for i in range (nbr):
     #add 
     c=a+b
     #after adding and printing should refresh variables by shift nbrs a will take value of b and b will take value of c 
